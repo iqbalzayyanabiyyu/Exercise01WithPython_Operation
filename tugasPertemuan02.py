a0288a9bef729e1f4f69118560cb0f26caf9f6c0
Iqbal Zayyan <iart9290@gmail.com>
2021-10-05 11:26:17 +0700

    First Commit

---
+++ tugasPertemuan02.py
@@ -0,0 +1,15 @@
+# STUDI KASUS TOKO BUAH
+# OWNER: M. IQBAL ZAYYAN ABIYYU
+# KELAS: 12.1A.39
+# NIM: 12210720
+print("TOKO BUAH SUPER SEGAR")
+print("*********************")
+# Harga Mangga Per KG
+hrgManggaPerKg = int(input("Harga Mangga Per KG:"))
+# Pembeli membeli mangga dengan berat 7 KG
+brtManggaPembeli = int(input("Berat Mangga Pembeli:"))
+totalHarga = hrgManggaPerKg * brtManggaPembeli
+print("Total Harga Mangga:", totalHarga)
+print("*********************")
+print("Terimakasih & Selamat Datang Kembali")
+print("Owner Toko: 12210720 M. IQBAL ZAYYAN ABIYYU")
